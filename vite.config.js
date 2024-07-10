91b349ffaf5bbf35031ccad9d25a864b1d89dee2
parvin <parvinnoori0@gmail.com>
2024-07-10 14:23:00 +0330

    config js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/quizApp/', // Set to your GitHub Pages repo name
+  build: {
+    outDir: 'dist', // Ensure the output directory is `dist`
+  },
 })
